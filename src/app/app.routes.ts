a60c102ce6cacc5faa569251477b276575e22d42
cabarronc <cabarronc@guanajuato.gob.mx>
2025-08-29 12:56:28 -0600

    Ajustes en interfaz por limpieza de modulos en desuso

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import {CircularComponent} from './views/circular/circular.component'
-import { PreguntasComponent } from './views/IA/preguntas/preguntas.component';
 import { MailComponent } from './views/mail/mail/mail.component';
 import { AdecuacionesComponent } from './views/adecuaciones/adecuaciones/adecuaciones.component';
 import { CosaincegComponent } from './views/cosainceg/cosainceg.component';
@@ -10,7 +9,6 @@ import { ClaudeComponent } from './views/claude/claude.component';
 export const routes: Routes = [
     { path: 'principal', component: HomeComponent},
     { path: 'circular', component: CircularComponent},
-    { path: 'centro_datos', component: PreguntasComponent},
     { path: 'correos', component:  MailComponent},
     { path: 'adecuaciones', component:  AdecuacionesComponent},
     { path: 'cosainceg', component:  CosaincegComponent},
